38a1fd0bf337781790be1c0f671e81c0fefd908d
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-04-30 19:22:34 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,75 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    return song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    rows = filename.split(".mp3")
+    album = rows.collect do |row|
+      data = row.split(" - ")
+      artist_name = data[0]
+      name = data[1]
+
+      song = self.new
+      song.artist_name = artist_name
+      song.name = name
+      song.save
+      song
+    end
+    album[0]
+  end
+
+  def self.create_from_filename(filename)
+    rows = filename.split(".mp3")
+    album = rows.collect do |row|
+      data = row.split(" - ")
+      artist_name = data[0]
+      name = data[1]
+
+      song = self.new
+      song.artist_name = artist_name
+      song.name = name
+      song.save
+      song
+    end
+    album[0]
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+  # binding.pry
+
 end
+
+# song = Song.new_by_name("Coming Home")
+# song.name
