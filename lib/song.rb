8d618fb6b43dd90874c8218ed08f37045daf36b1
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-01 22:59:04 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,50 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|songs| songs.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.empty? == true
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|word| word.name}
+  end
+
+  def self.new_from_filename(name)
+    song = self.name
+    song.name = name
+    song.name
+    binding.pry
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
