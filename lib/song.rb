030aa1f07e86a8db8118556bc71e9e272977aac3
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-02 21:41:47 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if @@all.empty? == true
+    if self.find_by_name(name) == nil
       self.create_by_name(name)
     else
       self.find_by_name(name)
@@ -53,6 +53,21 @@ class Song
     song.name = name
     song.name
     binding.pry
+  end
+
+  def self.new_from_filename(name)
+    song = self.new
+    song.name = name.split(" - ")[1].chomp(".mp3")
+    song.artist_name = name.split(" - ")[0]
+    song
+  end
+
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = name.split(" - ")[1].chomp(".mp3")
+    song.artist_name = name.split(" - ")[0]
+    @@all << song
+    song
   end
 
   def self.destroy_all
