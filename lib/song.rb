335701ea6c3b4798af3ed49faf06f511e614d3af
chica25 <patricia@patriciaco.com>
2020-05-25 00:00:58 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -11,15 +12,15 @@ class Song
   end
 
   def self.create
-    song = self.new
-    self.all << song
-    song
+   song = self.new
+   self.all << song
+   song
   end
 
   def self.new_by_name(name)
-    song = self.new
-    song.name = name
-    song
+   song = self.new
+   song.name = name
+   song
   end
 
   def self.create_by_name(name)
@@ -30,15 +31,15 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.find {|song| song.name == name}
+    self.all.detect { |song| song.name == name }
   end
 
   def self.find_or_create_by_name(name)
-    Song.find_by_name(name) == nil ? Song.create_by_name(name) : Song.find_by_name(name)
+   self.find_by_name(name) == nil ? self.create_by_name(name) : self.find_by_name(name)
   end
 
   def self.alphabetical
-    alpha = self.all.sort_by {|song| song.name}
+    alpha = self.all.sort_by { |song| song.name }
     alpha
   end
 
@@ -59,8 +60,10 @@ class Song
     song
   end
 
+
   def self.destroy_all
     self.all.clear
   end
 
-end
+ end
+
